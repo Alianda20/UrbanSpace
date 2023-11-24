2634dd2872ed932bef81a5e95d150b3f96091d37
Alianda20 <kelalianda@gmail.com>
2023-11-24 21:28:12 +0300

    Initial commit

---
+++ listings/choices.py
@@ -0,0 +1,48 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '10M':'Ksh10M',
+  '20M':'Ksh20M',
+  '30M':'Ksh30M',
+  '40M':'KsH40M',
+  '50M':'Ksh50M',
+  '60M':'Ksh60M',
+  '70M':'Ksh70M',
+  '80M':'Ksh80M',
+  '90M':'Ksh90M',
+  '100M':'Ksh100M+',
+}
+
+state_choices = {
+  'NBO':'Nairobi',
+  'MBS':'Mombasa',
+  'KSM':'Kisumu',
+  'ELD':'Eldoret',
+  'NKR':'Nakuru',
+  'THI':'Thika',
+  'MUM':'Muranga',
+  'NRI':'Nyeri',
+  'KTN':'Kitale',
+  'MKU':'Machakos',
+  'KTR':'Kericho',
+  'KLB':'Kilifi',
+  'MBU':'Malindi',
+  'GTS':'Garissa',
+  'EMB':'Embu',
+  'HOM':'Homabay',
+  'ISL':'Isiolo',
+  'KSG':'Kisii',
+  'MUR':'Meru',
+  'VOI':'Voi',
+}
